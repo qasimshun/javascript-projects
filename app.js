d793ab052e6a76cabc0f379da23744dad3966afa
qasimshun <63591828+qasimshun@users.noreply.github.com>
2021-11-13 07:48:47 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,72 +1,44 @@
-let userScore = 0;
-let compScore = 0;
-const userScore_span = document.getElementById("user");
-const compScore_span = document.getElementById("comp");
-const scoreBox_div = document.querySelector(".score_box");
-const result_h1 = document.getElementById("statement");
-const rock_div = document.getElementById("rock");
-const paper_div = document.getElementById("paper");
-const scissors_div = document.getElementById("scissors");
-
-
-
-function computerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    const random = Math.floor(Math.random()*3);
-    return choices[random]; 
-}
-
-function win(userChoice, compChoice) {
-    userScore++; 
-    userScore_span.innerHTML = userScore;
-    compScore_span.innerHTML = compScore;
-    result_h1.innerHTML = "You Won!!";
-}
-function lose(userChoice, compChoice) {
-    compScore++; 
-    userScore_span.innerHTML = userScore;
-    compScore_span.innerHTML = compScore;
-    result_h1.innerHTML = "computer wins";
-}
-function draw(userChoice, compChoice) {
-    result_h1.innerHTML = "It is draw!!";
-}
-
-function game(userChoice) {
-    const compChoice = computerChoice();
-    switch (compChoice + userChoice) {
-        case "paperrock":
-        case "rockscissors":
-        case "scissorspaper":
-            win();
-            break;
-        case "rockpaper":
-        case "paperscissors":
-        case "scissorsrock":
-            lose();
-            break;
-        case "rockrock":
-        case "scissorsscissors":
-        case "paperpaper":
-            draw();
-            break;           
+const leftBtn = document.querySelector('.left');
+const pauseBtn = document.querySelector('.pause');
+const rightBtn = document.querySelector('.right');
+const title = document.querySelector('.title');
+const audio = document.querySelector('.audio');
+const image = document.querySelector('.img')
+let isPlaying = false;
+let counter = 0;
+let nameOfTheSong = ['anewbeginning','creativeminds','ukulele'];
+loadSong(nameOfTheSong[counter]);
+pauseBtn.addEventListener('click', () => {
+    if(isPlaying){
+        audio.pause();
+        isPlaying = false;
+    }else {
+        audio.play();
+        isPlaying = true;
     }
-}
+});
 
-function main() {
+leftBtn.addEventListener('click', ()=>{
     
-rock_div.addEventListener("click", function(){
-    game("rock");
+    counter--;
+    loadSong(nameOfTheSong[counter])
+    if(counter<0){
+        counter=2;
+    }
+    audio.play()
+});
+
+rightBtn.addEventListener('click', ()=>{
+    
+    counter++;
+    loadSong(nameOfTheSong[counter])
+    if(counter>nameOfTheSong.length-1){
+        counter=0;
+    }
+    audio.play()
 })
 
-paper_div.addEventListener("click", function(){
-    game("paper");
-})
-
-scissors_div.addEventListener("click", function(){
-    game("scissors");
-})
+function loadSong(song){
+    audio.src = `bensound-${song}.mp3`;
+    image.src = `${song}.jpg`;
 }
-
-main();
-
